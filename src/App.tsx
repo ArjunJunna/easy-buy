032e340f6c8390ac0b481b7102e4b7a7185e84ae
Arjun Junna <arjun9852@gmail.com>
2023-05-06 15:46:28 +0530

    feat : initial setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h2 className='bg-slate-800 text-white'>Easy Buy is an ecommerce app built with React,Redux-Toolkit,Tailwind CSS and TypeScript</h2>
+      </div>
+    </>
+  );
+}
+
+export default App
