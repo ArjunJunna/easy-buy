527dde973e637ef0b778f98a1e9adf4cee57ff7d
Arjun Junna <arjun9852@gmail.com>
2023-09-10 19:31:34 +0530

    refactor : disable devtool

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,11 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import store from './store.ts';
 import { Provider } from 'react-redux';
+import { disableReactDevTools } from '@fvilers/disable-react-devtools';
 
-
+if (process.env.NODE_ENV === 'production') {
+  disableReactDevTools();
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
